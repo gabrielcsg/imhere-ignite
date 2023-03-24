2b3f9a804956e489599484250c28b0a98214d133
Gabriel Castro <gabrielcssg@gmail.com>
2023-03-24 10:02:37 -0300

    feat: participants list

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,63 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+
 import { styles } from "./styles";
 
-const Home: React.FC = () => {
+import { Participant } from "../../components/Participant";
+
+export function Home() {
+  const participants = [
+    "Gabriel",
+    "Samara",
+    "Lucky",
+    "Layla",
+    "Lupita",
+    "Leona",
+    "jose",
+    "mayke",
+    "teste",
+  ];
+
+  function handleParticipantAdd() {
+    console.log("adicionar");
+  }
+
+  function handleParticipantRemove(name: string) {
+    console.log("remove", name);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 24 de março de 2023.</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {participants.map((participant) => (
+          <Participant
+            key={participant}
+            name={participant}
+            onRemove={handleParticipantRemove}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
-};
-
-export { Home };
+}
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,18 +2,46 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#131016",
+    flex: 1,
     padding: 24,
   },
   eventName: {
+    color: "#FFF",
     fontSize: 24,
     fontWeight: "700",
-    color: "#FDFCFE",
     marginTop: 48,
   },
   eventDate: {
-    fontSize: 16,
     color: "#6B6B6B",
+    fontSize: 16,
+  },
+  form: {
+    flexDirection: "row",
+    marginBottom: 42,
+    marginTop: 36,
+    width: "100%",
+  },
+  input: {
+    backgroundColor: "#1f1e25",
+    borderRadius: 5,
+    color: "#FFF",
+    flex: 1,
+    fontSize: 16,
+    height: 56,
+    padding: 16,
+    marginRight: 12,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#31CF67",
+    borderRadius: 5,
+    justifyContent: "center",
+    height: 56,
+    width: 56,
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 24,
   },
 });
